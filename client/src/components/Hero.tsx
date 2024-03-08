8b839f42168d88dc6c925713602569aaa7aa6cd6
Rishabh Singh <rishabhsinghcodes@gmail.com>
2024-03-09 00:51:14 +0530

    added header & footer

---
+++ client/src/components/Hero.tsx
@@ -0,0 +1,16 @@
+const Hero = () => {
+  return (
+    <div className="bg-blue-600 pb-16">
+      <div className="container mx-auto flex flex-col gap-2">
+        <h1 className="text-5xl text-white font-bold">
+          Reserve your next stay
+        </h1>
+        <p className="text-2xl text-white">
+          Search low prices & high quality hotels for your next trip.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
