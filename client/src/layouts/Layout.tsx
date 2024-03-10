595cda25a817547660befd956bc6fb40ba6281b2
Rishabh Singh <rishabhsinghcodes@gmail.com>
2024-03-10 21:25:56 +0530

    added registration functionality

--- client/src/layouts/Layout.tsx
+++ client/src/layouts/Layout.tsx
@@ -6,7 +6,7 @@ interface Props {
   children: React.ReactNode;
 }
 
-const Layout = ({children}: Props) => {
+const Layout = ({ children }: Props) => {
   return (
     <div className="flex flex-col min-h-screen font-poppins">
       <Header />
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import userRoutes from "./routes/users";
+import authRoutes from "./routes/auth";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
@@ -10,9 +12,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
 
-app.get("/api/test", async (req: Request, res: Response) => {
-    res.json({message: "Hello form express server!"});
-});
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(8080, () => {
     console.log("Server running on port:8080");
