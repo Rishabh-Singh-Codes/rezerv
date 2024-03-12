7fa723020a30d1120f6d608a6bd440b1c7144efc
Rishabh Singh <rishabhsinghcodes@gmail.com>
2024-03-12 21:11:29 +0530

    added cookies functionality

--- client/src/api-client.ts
+++ client/src/api-client.ts
@@ -5,6 +5,7 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export const register = async (formData: RegisterFormData) => {
   const response = await fetch(`${API_BASE_URL}/api/users/register`, {
     method: "POST",
+    credentials: "include",
     headers: {
       "Content-Type": "application/json",
     },
@@ -13,7 +14,19 @@ export const register = async (formData: RegisterFormData) => {
 
   const responseBody = await response.json();
 
-  if(!response.ok) {
+  if (!response.ok) {
     throw new Error(responseBody.message);
   }
+};
+
+export const validateToken = async () => {
+  const response = await fetch(`${API_BASE_URL}/api/auth/validate-token`, {
+    credentials: "include",
+  });
+
+  if (!response.ok) {
+    throw new Error("Token invalid");
+  }
+
+  return response.json();
 };
