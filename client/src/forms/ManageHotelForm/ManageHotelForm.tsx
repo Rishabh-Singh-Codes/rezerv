9d2a33f9e1d564454576a425e1b8afc31dc57925
Rishabh Singh <rishabhsinghcodes@gmail.com>
2024-03-21 21:44:20 +0530

    hotel form UI connected via API

--- client/src/forms/ManageHotelForm/ManageHotelForm.tsx
+++ client/src/forms/ManageHotelForm/ManageHotelForm.tsx
@@ -19,13 +19,38 @@ export type HotelFormData = {
   childCount: number;
 };
 
-const ManageHotelForm = () => {
-  const formMethods = useForm<HotelFormData>();
-  const {handleSubmit} = formMethods;
+type Props = {
+  onSave: (hotelFormData: FormData) => void;
+  isLoading: boolean;
+};
 
-  const onSubmit = handleSubmit((formData: HotelFormData) => {
-    console.log('formData', formData)
-  })
+const ManageHotelForm = ({ onSave, isLoading }: Props) => {
+  const formMethods = useForm<HotelFormData>();
+  const { handleSubmit } = formMethods;
+
+  const onSubmit = handleSubmit((formDataJson: HotelFormData) => {
+    const formData = new FormData();
+
+    formData.append("name", formDataJson.name);
+    formData.append("city", formDataJson.city);
+    formData.append("country", formDataJson.country);
+    formData.append("description", formDataJson.description);
+    formData.append("type", formDataJson.type);
+    formData.append("pricePerNight", formDataJson.pricePerNight.toString());
+    formData.append("starRating", formDataJson.starRating.toString());
+    formData.append("adultCount", formDataJson.adultCount.toString());
+    formData.append("childCount", formDataJson.childCount.toString());
+
+    formDataJson.facilities.forEach((facility, index) => {
+      formData.append(`facilities[${index}]`, facility);
+    });
+
+    Array.from(formDataJson.imageFiles).forEach((imageFile) => {
+      formData.append(`imageFiles`, imageFile);
+    });
+
+    onSave(formData);
+  });
 
   return (
     <FormProvider {...formMethods}>
@@ -38,9 +63,10 @@ const ManageHotelForm = () => {
         <span className="flex justify-end">
           <button
             type="submit"
-            className="bg-blue-400 text-white p-2 font-bold hover:bg-blue-500 text-lg rounded-md"
+            disabled={isLoading}
+            className="bg-blue-400 text-white p-2 font-bold hover:bg-blue-500 text-lg rounded-md disabled:bg-gray-400"
           >
-            Save
+            {isLoading ? "Saving..." : "Save"}
           </button>
         </span>
       </form>
--- client/src/pages/AddHotel.tsx
+++ client/src/pages/AddHotel.tsx
@@ -1,9 +1,24 @@
+import { useMutation } from "react-query";
 import ManageHotelForm from "../forms/ManageHotelForm/ManageHotelForm";
+import * as apiClient from "../api-client";
+import { useAppContext } from "../contexts/AppContext";
 
 const AddHotel = () => {
-    return (
-        <ManageHotelForm />
-    )
-}
+  const { showToast } = useAppContext();
+  const { mutate, isLoading } = useMutation(apiClient.addMyHotel, {
+    onSuccess: () => {
+      showToast({ type: "SUCCESS", message: "Hotel Saved!" });
+    },
+    onError: () => {
+      showToast({ type: "ERROR", message: "Error Saving Hotel!" });
+    },
+  });
+
+  const handleSave = (hotelFormData: FormData) => {
+    mutate(hotelFormData);
+  };
+
+  return <ManageHotelForm onSave={handleSave} isLoading={isLoading} />;
+};
 
 export default AddHotel;
