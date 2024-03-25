df5b7545aebfcf269ad3990db46a17f435d71af9
Rishabh Singh <rishabhsinghcodes@gmail.com>
2024-03-25 19:22:49 +0530

    added edit hotel feature

---
+++ client/src/pages/EditHotel.tsx
@@ -0,0 +1,37 @@
+import { useMutation, useQuery } from "react-query";
+import { useParams } from "react-router-dom";
+import * as apiClient from "../api-client";
+import ManageHotelForm from "../forms/ManageHotelForm/ManageHotelForm";
+import { useAppContext } from "../contexts/AppContext";
+
+const EditHotel = () => {
+  const { hotelId } = useParams();
+  const { showToast } = useAppContext();
+
+  const { data: hotel } = useQuery(
+    "fetchMyHotelById",
+    () => apiClient.fetchMyHotelById(hotelId || ""),
+    {
+      enabled: !!hotelId,
+    }
+  );
+
+  const { mutate, isLoading } = useMutation(apiClient.updateMyHotelById, {
+    onSuccess: () => {
+      showToast({ type: "SUCCESS", message: "Hotel updated successfully" });
+    },
+    onError: () => {
+      showToast({ type: "ERROR", message: "Error updating hotel" });
+    },
+  });
+
+  const handleSave = (hotelFormData: FormData) => {
+    mutate(hotelFormData);
+  };
+
+  return (
+    <ManageHotelForm hotel={hotel} onSave={handleSave} isLoading={isLoading} />
+  );
+};
+
+export default EditHotel;
