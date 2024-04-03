9edf328fadfc1a9853e1919b8b80966c6804ec4e
Rishabh Singh <rishabhsinghcodes@gmail.com>
2024-04-03 21:43:01 +0530

    added search hotel test

---
+++ e2e-tests/tests/search-hotel.spec.ts
@@ -0,0 +1,28 @@
+import {test, expect} from "@playwright/test"
+
+const UI_URL = "http://localhost:5173/";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(UI_URL);
+
+  await page.getByRole("link", { name: "Sign In" }).click();
+
+  await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible();
+
+  await page.locator("[name=email]").fill("5@5.com");
+  await page.locator("[name=password]").fill("qwerty");
+
+  await page.getByRole("button", { name: "Login" }).click();
+
+  await expect(page.getByText("Sign In Successful!")).toBeVisible();
+});
+
+test("should show hotel search results", async ({page}) => {
+    await page.goto(UI_URL);
+
+    await page.getByPlaceholder("Where are you going?").fill("Test city");
+    await page.getByRole("button", {name: "Search"}).click();
+
+    await expect(page.getByText("Hotels found in Test city")).toBeVisible();
+    await expect(page.getByText("Test Hotel").first()).toBeVisible();
+})
