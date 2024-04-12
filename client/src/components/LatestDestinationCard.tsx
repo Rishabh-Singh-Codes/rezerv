e370f44a0bb1fcd81860a8cd8e2b5350cb194de0
Rishabh Singh <rishabhsinghcodes@gmail.com>
2024-04-12 23:57:37 +0530

    updated destination card

--- client/src/components/LatestDestinationCard.tsx
+++ client/src/components/LatestDestinationCard.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { HotelType } from "../../../server/src/shared/types";
+import { FaStar } from "react-icons/fa6";
 
 type Props = {
   hotel: HotelType;
@@ -17,8 +18,21 @@ const LatestDestinationCard = ({ hotel }: Props) => {
           className="w-full h-full object-center object-cover rounded-lg"
         />
       </div>
-      <div className="absolute bottom-0 p-4 bg-black bg-opacity-50 w-full rounded-b-lg">
-        <span className="text-white font-bold tracking-tight text-3xl">{hotel.name}</span>
+      <div className="absolute group bottom-0 p-4 bg-black bg-opacity-50 w-full rounded-b-lg hover:h-full hover:bg-opacity-60 transition-colors ease-in-out flex flex-col">
+        <span className="text-white font-bold tracking-tight text-3xl block flex-1">
+          {hotel.name}
+        </span>
+        <span className="text-white font-bold tracking-tight text-xl hidden group-hover:block">
+          {hotel.city}, {hotel.country}
+        </span>
+        <span className="text-white font-bold tracking-tight text-lg hidden group-hover:block">
+          ₹ {hotel.pricePerNight} per night
+        </span>
+        <span className="text-white font-bold tracking-tight text-lg hidden group-hover:block">
+          {Array.from({ length: hotel.starRating }).map((_, idx) => (
+            <FaStar className="fill-orange-400 inline" key={idx} />
+          ))}
+        </span>
       </div>
     </Link>
   );
